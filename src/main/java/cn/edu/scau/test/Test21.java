e172f7bb62cd13febe6608053180e6a7838eede2
cyqa2001 <cyqa2001@gmail.com>
2023-05-05 20:58:20 +0800

    异步模式之生产者/消费者

--- src/main/java/cn/edu/scau/test/Test21.java
+++ src/main/java/cn/edu/scau/test/Test21.java
@@ -1,13 +1,37 @@
 package cn.edu.scau.test;
 
+import cn.edu.scau.n2.util.Sleeper;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * 生产者消费者
+ */
+@Slf4j(topic = "c.Test21")
 public class Test21 {
+    public static void main(String[] args) {
+        MessageQueue queue = new MessageQueue(2);
+        for (int i = 0; i < 3; i++) {
+            int id = i;
+            new Thread(() -> {
+                queue.put(new Message(id, "值" + id));
+            }, "生产者" + i).start();
+        }
+
+        new Thread(() -> {
+            while (true) {
+                Sleeper.sleep(1);
+                Message message = queue.take();
+            }
+        }, "消费者").start();
+    }
 }
 
 // 消息队列类, java线程之间通信
+@Slf4j(topic = "c.MessageQueue")
 class MessageQueue {
     // 消息的队列集合
     private LinkedList<Message> list = new LinkedList<>();
@@ -20,12 +44,41 @@ class MessageQueue {
 
     // 获取消息
     public Message take() {
-        // 
-        return null;
+        // 检查队列是否为空
+        synchronized (list) {
+            while (list.isEmpty()) {
+                try {
+                    log.debug("队列为空, 消费者线程等待");
+                    list.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // 从队列的头部获取消息返回
+            Message message = list.removeFirst();
+            log.debug("已消费消息 {}", message);
+            list.notifyAll();
+            return message;
+        }
     }
 
     // 存入消息
     public void put(Message message) {
+        synchronized (list) {
+            while (list.size() == capacity) {
+                try {
+                    log.debug("队列已满, 生产者线程等待");
+                    list.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 将消息加入队列尾部
+            list.addLast(message);
+            log.debug("已生产消息 {}", message);
+            list.notifyAll();
+        }
 
     }
 
